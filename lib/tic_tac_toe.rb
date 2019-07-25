4a7c050d14780706dffbdd7fbe0b8ea08f05cc75
Veronica Lopez <nicaa0695@gmail.com>
2019-07-25 21:53:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,5 +70,6 @@ class TicTacToe
   end
   
   def won?
-    WI
+    WIN_COMBINATIONS.find do |win_combination|
+      if @board[win_combination[0]] == "X" && @board[win_combination[1]] == "X" && 
 end
