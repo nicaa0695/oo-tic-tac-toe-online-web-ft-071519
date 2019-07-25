6a9078fec33370d0ffd5c8266a40110065c9631d
Veronica Lopez <nicaa0695@gmail.com>
2019-07-25 21:50:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,4 +58,14 @@ class TicTacToe
       turn
     end
   end
+  
+  def turn_count
+    count = 0 
+    @board.each do |token|
+      if token == "X" || token == "O"
+        count += 1 
+      end
+    end
+    return count
+  end
 end
