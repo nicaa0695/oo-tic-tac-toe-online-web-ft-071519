d102e9d8d0e2d3f453fd630bd4965714b221985e
Veronica Lopez <nicaa0695@gmail.com>
2019-07-25 21:27:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,6 +16,8 @@ class TicTacToe
   def display_board
     row = '-----------'
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ""
+    puts row 
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts row
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
